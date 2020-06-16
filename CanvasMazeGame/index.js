08dabc553a7777340c029274a4ba7dc92b899454
Epinder <epinder.singh@gmail.com>
2020-06-17 07:57:21 +0800

    Maze game still progress

--- CanvasMazeGame/index.js
+++ CanvasMazeGame/index.js
@@ -29,6 +29,21 @@ World.add(world, walls);
 
 //Maze generation
 
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+};
+
 const grid = Array(cells)
   .fill(null) //does not matter what to put in here will be replaced in next line
   .map(() => Array(cells).fill(false));
@@ -54,22 +69,47 @@ const stepThroughCell = (row, column) => {
   grid[row][column] = true;
 
   //Assemble randomly-ordered list of neighbours
-  const neighbours = [
-    [row - 1, column],
-    [row, column + 1],
-    [row + 1, column],
-    [row, column - 1],
-  ];
+  const neighbours = shuffle([
+    [row - 1, column, 'up'],
+    [row, column + 1, 'right'],
+    [row + 1, column, 'down'],
+    [row, column - 1, 'left'],
+  ]);
+
   //For each neighbours....
+  for (let neighbour of neighbours) {
+    const [nextRow, nextColumn, direction] = neighbour;
+    //See if thatr neighbour is out of bounds
+    if (
+      nextRow < 0 ||
+      nextRow >= cells ||
+      nextColumn < 0 ||
+      nextColumn >= cells
+    ) {
+      continue;
+    }
 
-  //See if thatr neighbour is out of bounds
+    //If we have visited that neighbour, continue to next neighbour
 
-  //If we have visited that neighbour, continue to next neighbour
+    if (grid[nextRow][nextColumn]) {
+      continue;
+    }
 
-  //Remove a wall from either horizontals or verticals
+    //Remove a wall from either horizontals or verticals
 
+    if (direction === 'left') {
+      verticals[row][column - 1] = true;
+    } else if (direction === 'right') {
+      verticals[row][column] = true;
+    } else if (direction === 'up') {
+      horizontals[row - 1][column] = true;
+    } else if (direction === 'down') {
+      horizontals[row][column] = true;
+    }
+
+    stepThroughCell(nextRow, nextColumn);
+  }
   // visit that next cell
 };
 
 stepThroughCell(startRow, startColumn);
-console.log(grid);
