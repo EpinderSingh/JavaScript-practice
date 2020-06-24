b91e53b37c1a3dea48b941556b6e9270d62437b6
Epinder <epinder.singh@gmail.com>
2020-06-24 14:49:35 +0800

    Complete WatchIt App

--- watchit/index.js
+++ watchit/index.js
@@ -2,13 +2,36 @@
 
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a fole to execute')
+  .action(async ({ filename }) => {
+    const name = filename || index.js;
 
-const start = debounce(() => {
-  console.log('STARTING USER PROGRAM');
-}, 100);
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
 
-chokidar
-  .watch('.')
-  .on('add', start)
-  .on('change', () => console.log('File Changed'))
-  .on('unlink', () => console.log('File Unliked'));
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.bgYellow('>>>>>>> Starting proces....'));
+      proc = spawn('node', [name], { stdio: 'inherit' });
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
